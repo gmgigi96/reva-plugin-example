8a899f055979c282988cbbfd1f538c70b4ee867e
Gianmaria Del Monte <g.macmount@gmail.com>
2023-07-19 12:00:45 +0200

    plugin

---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/gmgigi96/reva-plugin-example
+
+go 1.20
+
+require github.com/cs3org/reva v1.24.0
+
+require (
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.11.2 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/rs/zerolog v1.28.0 // indirect
+	golang.org/x/crypto v0.8.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+)
+
+replace github.com/cs3org/reva => /home/gianmaria/Documenti/CERN/reva2
---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/cs3org/reva"
+	"github.com/cs3org/reva/pkg/appctx"
+	"github.com/cs3org/reva/pkg/rhttp/global"
+	"github.com/cs3org/reva/pkg/utils/cfg"
+)
+
+func init() {
+	reva.RegisterPlugin(HelloWorld{})
+}
+
+type HelloWorld struct {
+	c *config
+}
+
+type config struct {
+	Name string `mapstructure:"name"`
+}
+
+func (c *config) ApplyDefaults() {
+	if c.Name == "" {
+		c.Name = "world"
+	}
+}
+
+func (HelloWorld) RevaPlugin() reva.PluginInfo {
+	return reva.PluginInfo{
+		ID:  "http.services.helloplugin",
+		New: New,
+	}
+}
+
+func New(ctx context.Context, m map[string]any) (global.Service, error) {
+	var c config
+	if err := cfg.Decode(m, &c); err != nil {
+		return nil, err
+	}
+	return &HelloWorld{c: &c}, nil
+}
+
+func (s *HelloWorld) Handler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log := appctx.GetLogger(r.Context())
+		msg := fmt.Sprintf("Hello %s from a reva plugin!", s.c.Name)
+		if _, err := w.Write([]byte(msg)); err != nil {
+			log.Err(err).Msg("error writing response")
+			return
+		}
+		log.Debug().Msgf("replied %s", msg)
+	})
+}
+
+func (s *HelloWorld) Prefix() string {
+	return "/helloplugin"
+}
+
+func (s *HelloWorld) Close() error { return nil }
+
+func (s *HelloWorld) Unprotected() []string {
+	return []string{"/"}
+}
+
+// guards to ensure HelloWorld implements a reva http service
+var _ global.Service = (*HelloWorld)(nil)
+var _ global.NewService = New
